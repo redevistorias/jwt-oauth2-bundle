c490cd8c6a74afdb972a70f6f5ebedd49876f54d
Henrique Becker <henrique.becker@redevistorias.com.br>
2026-03-06 10:15:52 -0300

    refactor: add return type declaration to load method in JwtOAuth2Extension

--- DependencyInjection/JwtOAuth2Extension.php
+++ DependencyInjection/JwtOAuth2Extension.php
@@ -9,7 +9,7 @@ use Symfony\Component\Config\FileLocator;
 
 class JwtOAuth2Extension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
